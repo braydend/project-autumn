694ba3b7a28b5c529dd769adeeafcb46f9b3b82c
Brayden <braydendiluca@gmail.com>
2020-03-05 20:59:08 +1100

    Add initial files

---
+++ spacebucket-web/src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import Sensors from "./components/Sensors";
+
+const AppContainer = styled.div`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+`;
+
+function App() {
+    return (
+        <AppContainer>
+            <header className="App-header">
+                <h1>Spacebucket sensors</h1>
+            </header>
+            <Sensors />
+        </AppContainer>
+    );
+}
+
+export default App;
