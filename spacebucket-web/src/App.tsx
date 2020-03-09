28827bd4d239c32d75cd1d6a0747e49286bcc46f
Brayden <braydendiluca@gmail.com>
2020-03-10 08:29:30 +1100

    test netlify env

--- spacebucket-web/src/App.tsx
+++ spacebucket-web/src/App.tsx
@@ -14,6 +14,7 @@ const AppContainer = styled.div`
 `;
 
 function App() {
+    console.table('env vars:', process.env)
     return (
         <AppContainer>
             <header className="App-header">
