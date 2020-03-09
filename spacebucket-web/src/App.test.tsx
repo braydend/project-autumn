95e81bdbab449bdfc5f26fd0a376b1fd39a1699c
Brayden <braydendiluca@gmail.com>
2020-03-09 19:18:24 +1100

    Fix all linting errors

--- spacebucket-web/src/App.test.tsx
+++ spacebucket-web/src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders heading', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Spacebucket sensors/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders heading", () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/Spacebucket sensors/i);
+    expect(linkElement).toBeInTheDocument();
 });
