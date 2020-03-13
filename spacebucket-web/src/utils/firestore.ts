8cceb26e2eb08e7ac6b6e5acafb7bcf2719669f3
Brayden <braydendiluca@gmail.com>
2020-03-14 09:00:35 +1100

    Add firebase utils

---
+++ spacebucket-web/src/utils/firestore.ts
@@ -0,0 +1,7 @@
+import firebase from "firebase";
+
+const getFirestoreInstance = (
+    connection: firebase.app.App
+): firebase.firestore.Firestore => connection.firestore();
+
+export { getFirestoreInstance };
