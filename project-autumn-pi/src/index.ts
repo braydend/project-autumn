d04ac4a89d599d96ce77470bbcef830ec3983bda
Brayden <braydendiluca@gmail.com>
2020-04-02 10:52:47 +1100

    Add test code for pushing data to Firestore

--- project-autumn-pi/src/index.ts
+++ project-autumn-pi/src/index.ts
@@ -4,7 +4,7 @@ import { isEnvironmentValid } from "./utils/utils";
 import { getFirestoreInstance } from "./utils/firestore";
 import { getFirebaseConnection } from "./utils/firebase";
 import { requiredEnv } from "./consts/env";
-import { SensorData } from "./types/Sensor";
+import Sensor, { SensorData } from "./types/Sensor";
 
 // Creates environment variables from .env
 config();
@@ -17,21 +17,25 @@ const firestore: firebase.firestore.Firestore = getFirestoreInstance(
   getFirebaseConnection()
 );
 
-const eventLoop: () => void = async () => {
-  const sensorData: SensorData[] = [];
-  await firestore
-    .collection("sensors")
-    .get()
-    .then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-        const { name, value } = doc.data();
-        const typedData: SensorData = { name, value };
-        sensorData.push(typedData);
-      });
-    });
-  console.table(sensorData);
-};
+// const eventLoop: () => void = async () => {
+//   const sensorData: SensorData[] = [];
+//   await firestore
+//     .collection("sensors")
+//     .get()
+//     .then((querySnapshot) => {
+//       querySnapshot.forEach((doc) => {
+//         const { name, value } = doc.data();
+//         const typedData: SensorData = { name, value };
+//         sensorData.push(typedData);
+//       });
+//     });
+//   console.table(sensorData);
+// };
 
-const app: CronJob = new CronJob("* * * * * *", eventLoop);
+// const app: CronJob = new CronJob("* * * * * *", eventLoop);
 
-app.start();
+// app.start();
+
+const sensor = new Sensor("foo-sensor", "foobar", firestore);
+
+sensor.storeData({ value: 1, timestamp: 12345 });
