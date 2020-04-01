9872dbb5ea7388e60b5cf5f060995a0c00a8ffb9
Brayden <braydendiluca@gmail.com>
2020-04-01 18:33:43 +1100

    Copy utils from web app

---
+++ project-autumn-pi/src/utils/utils.ts
@@ -0,0 +1,20 @@
+const checkEnvironment = (
+  env: NodeJS.ProcessEnv,
+  requiredVars: string[]
+): void => {
+  requiredVars.forEach((envVar: string) => {
+    if (!env[envVar]) throw new Error(`Mising environent variable: ${envVar}`);
+  });
+};
+
+const isEnvironmentValid = (
+  env: NodeJS.ProcessEnv,
+  requiredVars: string[]
+): boolean => {
+  checkEnvironment(env, requiredVars);
+  return requiredVars.reduce((acc: boolean, cur) => {
+    return env[cur] ? acc : false;
+  }, true);
+};
+
+export { isEnvironmentValid, checkEnvironment };
