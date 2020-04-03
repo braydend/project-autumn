c32a61c1ae9cc10238490f70c51618ce876f3d93
Brayden <braydendiluca@gmail.com>
2020-04-03 12:15:13 +1100

    Create Map of DS18B20 ids and names

---
+++ project-autumn-pi/src/consts/ds18b20.ts
@@ -0,0 +1,6 @@
+// Keys are the DS18B20 sensor ids
+export const sensorIdNameMap: Map<string, string> = new Map([
+  ["28-0316a30797ff", "Soil Temperature"],
+  ["28-0516a300f5ff", "Internal Air Temperature"],
+  ["28-0516a323b6ff", "External Air Temperatre"],
+]);
