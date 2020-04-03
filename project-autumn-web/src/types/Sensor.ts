cdecef7e3088daf0695ceca1c0a11e7293685e79
Brayden <braydendiluca@gmail.com>
2020-04-03 15:15:15 +1100

    Create Sensor Class

--- project-autumn-web/src/types/Sensor.ts
+++ project-autumn-web/src/types/Sensor.ts
@@ -1,6 +1,59 @@
-interface Sensor {
-    name: string;
-    value: boolean;
-}
+export type SensorData = { value: number; timestamp: number };
 
-export default Sensor;
+export default class Sensor {
+    private id: string;
+    private name: string;
+    private connection: firebase.firestore.Firestore;
+
+    constructor(
+        id: string,
+        name: string,
+        connection: firebase.firestore.Firestore
+    ) {
+        this.id = id;
+        this.name = name;
+        this.connection = connection;
+    }
+
+    public getId(): string {
+        return this.id;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public async fetchData(): Promise<SensorData[] | false> {
+        try {
+            let data: SensorData[] = [];
+            await this.connection
+                .collection("sensors")
+                .doc(this.id)
+                .collection("data")
+                .get()
+                .then(snap => {
+                    snap.forEach(doc => {
+                        const { value, timestamp } = doc.data();
+                        data.push({ value, timestamp });
+                    });
+                });
+            return data;
+        } catch (exception) {
+            console.error(exception);
+        }
+        return false;
+    }
+
+    public async getLastestData(): Promise<SensorData> {
+        const latest = await this.fetchData().then(data => {
+            if (data) {
+                return data.reduce<SensorData>(
+                    (acc, cur) => (cur.timestamp > acc.timestamp ? cur : acc),
+                    { value: 0, timestamp: 0 }
+                );
+            }
+        });
+        if (!latest) throw Error("No data");
+        return latest;
+    }
+}
