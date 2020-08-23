c31cb4d676cb5cfe2d0621b81d1976ae55d138d5
Brayden <braydendiluca@gmail.com>
2020-08-23 05:03:16 +0100

    Clean up sensor reading and reporting

--- project-autumn-pi/src/index.ts
+++ project-autumn-pi/src/index.ts
@@ -1,51 +1,13 @@
 import { config } from "dotenv";
-import Sensor, { DS18B20, SensorType } from "./types/Sensor";
-import {
-  transformDS18B20ArrayToSensorArray,
-} from "./utils/ds18b20";
-import { sendData } from "./utils/utils";
+import { readAndPersistTemperatures } from "./utils/ds18b20";
+import { readAndPersistMoistureSensor } from "./utils/moistureSensor";
 
 // Doesnt have types or use ESM :(
-// eslint-disable-next-line
-const ds18b20Raspi = require("ds18b20-raspi");
 // eslint-disable-next-line
 const cron = require("node-cron");
 
 // Creates environment variables from .env
 config();
-
-const readAndPersistTemperatures: () => void = async () => {
-  const allDS18B20Sensors: DS18B20[] = ds18b20Raspi.readAllC();
-
-  const ds18b20Sensors: Sensor[] = transformDS18B20ArrayToSensorArray(
-    allDS18B20Sensors,
-  );
-
-  for (const sensor of ds18b20Sensors) {
-    await sendData(sensor);
-  }
-
-  // ds18b20Sensors.forEach((sensor: Sensor) => {
-  //   // Send request to endpoint with id, type and reading
-
-  //   // sensor.storeData(getDataForSensor(sensor, allDS18B20Sensors));
-  // });
-};
-
-const readAndPersistMoistureSensor: () => void = async () => {
-  // The data pin connected to soil moiture sensor
-  const GPIO = 26;
-  const moistureSensor: Sensor = {
-    id: 'moisture-sensor-1',
-    name: 'Soil Moisture',
-    type: SensorType.Moisture,
-
-  };
-  // const moistureReading = getSensorDataForMoistureSensor(GPIO);
-
-  // Send request to endpoint with id, type and reading
-  await sendData(moistureSensor);
-};
 
 const main = () => {
   console.log("Running project-autumn...");
--- project-autumn-pi/src/utils/moistureSensor.ts
+++ project-autumn-pi/src/utils/moistureSensor.ts
@@ -1,5 +1,6 @@
 import { Gpio } from "onoff";
-import { SensorData } from "../types/Sensor";
+import Sensor, { SensorData, SensorType } from "../types/Sensor";
+import { sendData } from "./utils";
 
 export const SOIL_MOISTURE_MIN = 0;
 export const SOIL_MOISTURE_MAX = 0;
@@ -14,3 +15,17 @@ export const getSensorDataForMoistureSensor = (GPIO: number): SensorData => {
     const value: MoistureSensorStates.WET | MoistureSensorStates.DRY = sensor.readSync();
     return { value, timestamp: Date.now() };
 };
+
+export const readAndPersistMoistureSensor: () => void = async () => {
+    // The data pin connected to soil moiture sensor
+    const GPIO = 26;
+    const moistureSensor: Sensor = {
+      id: 'moisture-sensor-1',
+      name: 'Soil Moisture',
+      type: SensorType.Moisture,
+      reading: getSensorDataForMoistureSensor(GPIO),
+    };
+  
+    // Send request to endpoint with id, type and reading
+    await sendData(moistureSensor);
+  };
